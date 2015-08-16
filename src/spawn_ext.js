728888cbc0d0db416fc086913a78e3fa501e1ddb
edggy <edggy@Ethan-Alienware>
2015-08-16 18:51:22 -0400

    Oops

--- src/spawn_ext.js
+++ src/spawn_ext.js
@@ -18,11 +18,11 @@ Spawn.prototype.createWorkerCreep = function(body, name, memory) {
 Spawn.prototype.createLongestCreep = function(body, name, memory) {
 	memory = memory || {};
 	if(memory.spawn == undefined) memory.spawn = this.id;
-	var new_name = name + ' x' + body.length + '#' + Math.floor(Math.random()*1000);
+	var new_name = name + ' x' + body.length + ' #' + Math.floor(Math.random()*1000);
 	var err = this.canCreateCreep(body, new_name);
     while(err < 0 && body.length > 3) {
     	body.pop();
-    	new_name = name + ' x' + body.length + '#' + Math.floor(Math.random()*1000);
+    	new_name = name + ' x' + body.length + ' #' + Math.floor(Math.random()*1000);
     	err = this.canCreateCreep(body, new_name);
     }
     if(err == 0) {
