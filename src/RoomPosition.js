a5c003981b299d04976641ee5749bd295a68442f
edggy <edggy@Ethan-Alienware>
2015-08-17 19:39:34 -0400

    Bug Fix

--- src/RoomPosition.js
+++ src/RoomPosition.js
@@ -20,8 +20,8 @@ Object.defineProperty(RoomPosition.prototype, "memory", {
 	enumerable: true,
     get: function() {
     	var name = this.x+','+this.y
-    	Util.setUp(this.room.map, this.toString());
-    	return this.room.map[this.toString()];
+    	Util.setUp(Memory.rooms[this.roomName], this.toString());
+    	return Memory.rooms[this.roomName][this.toString()];
     }
 });
 
